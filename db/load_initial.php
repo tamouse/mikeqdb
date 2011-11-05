e51f3a9a7ea1a5fb5d5dcb4910e7da979e484090
Tamara Temple <tamouse@gmail.com>
2011-11-05 13:47:36 -0500

    minor problem in load_initial.php

--- db/load_initial.php
+++ db/load_initial.php
@@ -5,7 +5,7 @@
  *
  * @author Tamara Temple <tamara@tamaratemple.com>
  * @since 2011/11/05
- * @version <2011-Nov-05 13:00>
+ * @version <2011-Nov-05 13:46>
  * @copyright (c) 2011 Tamara Temple Web Development
  * @license GPLv3
  *
@@ -24,7 +24,6 @@ if (! file_exists($quote_file)) die ("$quote_file not found in ".APP_DIR."/db"."
 
 $contents = file_get_contents($quote_file);
 $quotes = explode("\n",  $contents);
-$dbg->p("quotes:",$quotes,__FILE__,__LINE__,__FUNCTION__);
 echo _wrap("Number of quotes in file: ".count($quotes),'div').PHP_EOL;
 
 
@@ -32,14 +31,14 @@ $sql = 'call add_quote(?);';
 $stmt = $qdb->prepare($sql) or
   die("SQL Statement prepare failed: (".$qdb->errno.") ".$qdb->error." SQL statement: $sql");
 
-echo '<ul>'.PHP_EOL;
+echo '<ol>'.PHP_EOL;
 foreach ($quotes as $index => $quote) {
-  echo _wrap("quote is ".htmlentities($quote),'li').PHP_EOL;
+  echo _wrap("quote [$index] is ".htmlentities($quote),'li').PHP_EOL;
   $stmt->bind_param('s',$quote) or
     die("bind_param failed: (".$stmt->errno.") ".$stmt->error." quote = $quote");
   $stmt->execute() or
     die("execute failed: (".$stmt->errno.") ".$stmt->error);
 }
-echo '</ul>'.PHP_EOL;
+echo '</ol>'.PHP_EOL;
 
 echo _wrap(total_quotes()." quotes in database.",'div').PHP_EOL;
